53ea936f248a3f4ed8d7a9e0dad0169e02834f60
Snaxexe <kiwi0507@outlook.com>
2022-08-31 14:21:40 +0200

    Added WriteToFile Function
    Reads from text files

--- ReadAndWrite/src/Main.cpp
+++ ReadAndWrite/src/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 
 namespace Amogus {
 
@@ -10,12 +11,24 @@ namespace Amogus {
 		std::cout << t << "\n";
 	}
 
+	void WriteToFile(std::string filepath)
+	{
+		std::ofstream file(filepath);
+		std::string FileContent;
+		std::getline(std::cin, FileContent);
+		file << FileContent;
+		file.close();
+	}
 
+
+	std::string filepath1 = ("myFile.txt");
 }
 
 
 
 int main() 
 {
-	Amogus::Print(2);
+	Amogus::Print("sus");
+
+	Amogus::WriteToFile(Amogus::filepath1);
 }
